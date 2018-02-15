f95899de978406024beaf0e28d1b1b6ae0ae36db
ryan <ryan@ryanjm.com>
2018-02-15 14:59:59 -0600

    Removing build files from git

--- Example/build/Pods.build/Debug-iphonesimulator/NumericKeyboard.build/Objects-normal/x86_64/NumericKeyboard-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/ryanjm/code/libs/NumericKeyboard/Example/Pods/Target\ Support\ Files/NumericKeyboard/NumericKeyboard-dummy.m \
-  /Users/ryanjm/code/libs/NumericKeyboard/Example/build/Pods.build/Debug-iphonesimulator/NumericKeyboard.build/module.modulemap \
-  /Users/ryanjm/code/libs/NumericKeyboard/Example/Pods/Target\ Support\ Files/NumericKeyboard/NumericKeyboard-prefix.pch
--- Example/build/Pods.build/Debug-iphonesimulator/NumericKeyboard.build/Objects-normal/x86_64/NumericKeyboard_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/ryanjm/code/libs/NumericKeyboard/Example/build/Pods.build/Debug-iphonesimulator/NumericKeyboard.build/DerivedSources/NumericKeyboard_vers.c
